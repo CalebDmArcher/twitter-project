1b67db55fe0d34b3f35406701bf9417acbebf5cd
Wu, Sitong <swu321@gatech.edu>
2022-05-05 04:19:12 -0400

    Install django restframework (bug fix)

--- twitter/urls.py
+++ twitter/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from accounts.api import views
+
 router = routers.DefaultRouter()
-router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
+router.register(r'api/users', views.UserViewSet)
+
 urlpatterns = [
  path('admin/', admin.site.urls),
  path('', include(router.urls)),
